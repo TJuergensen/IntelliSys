b3c1a3a8b65ecaa67fdc9b421ac1f4ccec6b1597
julip <juliparr94@gmail.com>
2018-11-26 15:45:12 +0100

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
