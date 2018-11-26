6035cc2c27d08e9b6986fbb9e8a00e9c1dd45633
julip <juliparr94@gmail.com>
2018-11-26 17:41:14 +0100

    Datenstructuren zum sammeln aller daten hinzugefügt. Berechnungen von daten hinzugefügt. Es fehlt eigentlich von den daten her nur noch die rundheit des huegels

--- src/Hill.java
+++ src/Hill.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Hill {
     private boolean isA;
     private final String colorA = "16776960";
@@ -5,19 +7,87 @@ class Hill {
     private String colorTop;
     private final String colorEndOfSlope = "0";
     private final String colorSlope = "16777215";
-    private double hilltopHigh;
+    private double hilltopHeight;
     private final int maxX = 3000;
     private final int maxY = 4943;
-    private final double minAvgHighDifference = 1.1;
+    private final double minAvgHeightDifference = 1.1;
     private final double maxDifferenceToHilltop = 2.0;
-    private final double avgSlopeDifferenceInit = 5.0;
     private String[][] data;
+    
+    //Structures to save all data
+    private ArrayList<Double> endOfHillHeights = new ArrayList();
+    private double minHeightHillEndPoint;
+    private double maxHeightHillEndPoint;
+    private double avgHeightHillEndPoints;
+
+    private int pointsOnHilltopCount = 0;
+    private int pointsOnSlopeCount = 0;
+    private int pointsAfterSlopeCount = 0;
+
+    private ArrayList<Integer> distancesHilltopAndEndOfHill = new ArrayList<>();
+    private double avgDistanceHilltopAndEndOfHill;
+
+    private ArrayList<Double> tilts = new ArrayList();
+    private double avgTilt;
+
+    private double relativeHilltopHeight;
 
     Hill(String[][] data, int x, int y, boolean isA) {
         this.isA = isA;
         this.data = data;
-        this.hilltopHigh = Double.parseDouble(data[x][y]);
+        this.hilltopHeight = Double.parseDouble(data[x][y]);
         markHill(x, y);
+        calculate();
+
+        System.out.println(
+        "\nisA                              : " + isA +
+        "\nminHeightHillEndPoint            : " + minHeightHillEndPoint +
+        "\nmaxHeightHillEndPoint            : " + maxHeightHillEndPoint +
+        "\navgHeightHillEndPoints           : " + avgHeightHillEndPoints +
+        "\npointsOnHilltopCount             : " + pointsOnHilltopCount +
+        "\npointsOnSlopeCount               : " + pointsOnSlopeCount +
+        "\npointsAfterSlopeCount            : " + pointsAfterSlopeCount +
+        "\navgDistanceHilltopAndEndOfHill   : " + avgDistanceHilltopAndEndOfHill +
+        "\navgTilt                          : " + avgTilt +
+        "\nrelativeHeightOfHill             : " + relativeHilltopHeight +
+        "\nheightOfHill                     : " + hilltopHeight
+        );
+    }
+
+    private void calculate() {
+        calculateAvgDistanceHilltopAndEndOfHill();
+        calculateAvgHeightBetweenBottomAndTop();
+        calculateMinAndMaxHeightBetweenBottomAndTop();
+        calculateAvgTilt();
+        relativeHilltopHeight = hilltopHeight - minHeightHillEndPoint;
+    }
+
+    private void calculateAvgTilt() {
+        this.avgTilt = this.tilts.stream().mapToDouble(d -> d).average().getAsDouble();
+    }
+
+    private void  calculateMinAndMaxHeightBetweenBottomAndTop() {
+        double min = 1000000000;
+        double max = 0;
+
+        for(double i : endOfHillHeights) {
+            if(i < min) {
+                min = i;
+            }
+            if(i > max) {
+                max = i;
+            }
+        }
+        this.minHeightHillEndPoint = min;
+        this.maxHeightHillEndPoint = max;
+    }
+
+    private void calculateAvgDistanceHilltopAndEndOfHill() {
+        this.avgDistanceHilltopAndEndOfHill = distancesHilltopAndEndOfHill.stream().mapToDouble(d -> d).average().getAsDouble();
+    }
+
+    private void calculateAvgHeightBetweenBottomAndTop() {
+        this.avgHeightHillEndPoints = endOfHillHeights.stream().mapToDouble(d -> d).average().getAsDouble();
     }
 
     private void markHill(int x, int y) {
@@ -31,52 +101,65 @@ class Hill {
         final int goLeft = -1;
         final int goUp = 1;
         final int goDown = -1;
+
+        int initDistance = 0;
         //right
-        markHill(x, y, goRight, stay, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, goRight, stay, this.hilltopHeight, initDistance);
         //left
-        markHill(x, y, goLeft,  stay, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, goLeft,  stay, this.hilltopHeight, initDistance);
         //up
-        markHill(x, y, stay,    goUp, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, stay,    goUp, this.hilltopHeight, initDistance);
         //down
-        markHill(x, y, stay,    goDown, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, stay,    goDown, this.hilltopHeight, initDistance);
         //up left
-        markHill(x, y, goLeft,  goUp, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, goLeft,  goUp, this.hilltopHeight, initDistance);
         //up right
-        markHill(x, y, goRight, goUp, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, goRight, goUp, this.hilltopHeight, initDistance);
         //down left
-        markHill(x, y, goLeft,  goDown, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, goLeft,  goDown, this.hilltopHeight, initDistance);
         //down right
-        markHill(x, y, goRight, goDown, this.avgSlopeDifferenceInit, this.hilltopHigh);
+        markHill(x, y, goRight, goDown, this.hilltopHeight, initDistance);
 
     }
 
-    private boolean markHill(int x, int y, int xOffset, int yOffset, double averageSlope, double oldHilltop) {
+    private boolean markHill(int x, int y, int xOffset, int yOffset, double oldPointOnHill, int distance) {
         //If the new Position in still in the data array do...
         y = y + yOffset;
         x = x + xOffset;
         if(y < this.maxY && y > 0 && x < this.maxX && x > 0) {
-            double newHilltop = Double.parseDouble(data[x][y]);
+            double newPointOnHill = Double.parseDouble(data[x][y]);
+
+            //If the next field is allready part of another hill
+            if(newPointOnHill == Double.parseDouble(colorEndOfSlope)) {
+                distancesHilltopAndEndOfHill.add(distance);
+                endOfHillHeights.add(oldPointOnHill);
+                return false;
+            }
             //Recognise if its still the part of the hilltop and mark it as such
-            if(newHilltop < this.hilltopHigh + this.maxDifferenceToHilltop && newHilltop > this.hilltopHigh - this.maxDifferenceToHilltop){
+            if(newPointOnHill < this.hilltopHeight + this.maxDifferenceToHilltop && newPointOnHill > this.hilltopHeight - this.maxDifferenceToHilltop){
                 data[x][y] = this.colorTop;
-                return markHill(x, y, xOffset, yOffset, averageSlope, newHilltop);
+                pointsOnHilltopCount++;
+                return markHill(x, y, xOffset, yOffset, newPointOnHill, ++distance);
             }else {
                 //Recognise if the new position is part of the slope
-                if (newHilltop < oldHilltop) {
-                    averageSlope = (averageSlope + (oldHilltop - newHilltop)) / 2;
+                if (newPointOnHill < oldPointOnHill) {
+                    Double tilt = (oldPointOnHill - newPointOnHill) / 2;
+                    tilts.add(tilt);
                     //Recognize if its still steep enough
-                    if (averageSlope < this.minAvgHighDifference) {
+                    if (tilt < this.minAvgHeightDifference) {
                         this.data[x][y] = this.colorEndOfSlope;
-                        return markHill(x, y, xOffset, yOffset, averageSlope, newHilltop);
+                        pointsAfterSlopeCount++;
+                        return markHill(x, y, xOffset, yOffset, newPointOnHill, ++distance);
                     } else {
+                        pointsOnSlopeCount++;
                         this.data[x][y] = this.colorSlope;
-                        return markHill(x, y, xOffset, yOffset, averageSlope, newHilltop);
+                        return markHill(x, y, xOffset, yOffset, newPointOnHill, ++distance);
                     }
-                } else {
-                    return false;
                 }
             }
         }
+        distancesHilltopAndEndOfHill.add(distance);
+        endOfHillHeights.add(oldPointOnHill);
         return false;
     }
 }
