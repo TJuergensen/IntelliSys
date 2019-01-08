d6136b8f7140e480f8892da4c86d911f66a6db56
julip <juliparr94@gmail.com>
2019-01-08 17:29:37 +0100

    eine magic number entfernt

--- src/Agent.java
+++ src/Agent.java
@@ -18,8 +18,8 @@ class Agent {
     private double falsePositive = 0.0; //count the false positive events in the real situations
     private final double quorumTP = 0.3; //The quorum that needs to be reached by the true positive rate
     private final double quorumFP = 0.2; //The quorum that needs to be reached by the false positive rate
-    private int minAmountOfAgentIds; //The min amount of agents that are used for evaluating the situation (35% of all agents)
-
+    private int minAmountOfAgentIds; //The min amount of agents that are used for evaluating the situation
+    private final double minAmountOfAgentIdsInPercent = 35; //The min amount of agents that are used for evaluating the situation in percent
     /**
      * Initialise the agent.
      * Creates the standardDeviation and the threshold as required in the task-position
@@ -27,7 +27,7 @@ class Agent {
      * @param agentCount The amount of all agents that will be created
      */
     Agent(int agentId, int agentCount) {
-        minAmountOfAgentIds = (int)((agentCount / 100.0) * 35);
+        minAmountOfAgentIds = (int)((agentCount / 100.0) * minAmountOfAgentIdsInPercent);
         this.agentId = agentId;
         this.agentsRating = new AgentMemory[agentCount];
         Arrays.fill(agentsRating, new AgentMemory());
